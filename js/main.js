f16c3953bdb289bef2899f804dbfac5f924505e9
s18009 <s18009@std.it-college.ac.jp>
2018-11-05 10:37:19 +0900

    アッセとの定義とプレイヤースプライトの表示

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ phina.globalize();
 
 const SCREEN_WIDTH = 960;
 const SCREEN_HEIGHT = 640;
+const ASSETS = {
+    "image": {
+        "buro": "./assetus/image/buropiyo.png",
+        "mero": "./assetus/image/meropiyo.png",
+        "mika": "./assetus/image/mikapiyo.png",
+        "nasu": "./assetus/image/nasupiyo.png",
+        "take": "./assetus/image/takepiyo.png",
+        "toma": "./assetus/image/tomapiyo.png",
+    }
+};
 
 phina.define('MainScene',{
     superClass: 'DisplayScene',
@@ -15,6 +25,12 @@ phina.define('MainScene',{
         this.gridY = Grid(SCREEN_HEIGHT, 40);
 
         this.backgroundColor = 'black';
+
+        const player = Sprite("toma", 64, 64).addChildTo(this);
+        player.setFrameIndex(10, 64, 64);
+        player.x = this.gridX.center();
+        player.y = this.gridY.span(37);
+
     }
 });
 
@@ -24,6 +40,7 @@ phina.main(() => {
         fps: 60,
         width: SCREEN_WIDTH,
         height: SCREEN_HEIGHT,
+        assets: ASSETS,
     });
     
     app.run();
